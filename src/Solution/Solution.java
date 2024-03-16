c33e722c6b46d2ff92b19967c98eb0f70c206ab1
juseungBaek <qkrwn37@naver.com>
2024-03-16 18:42:39 +0900

    1day

--- src/Solution/Solution.java
+++ src/Solution/Solution.java
@@ -1,5 +1,17 @@
 package Solution;
 
 public class Solution {
-
+    public String solution(String my_string, int[] indices) {
+        char[] arr = my_string.toCharArray();
+        for(int i : indices) {
+            arr[i] = ' ';
+        }
+        StringBuilder sb=  new StringBuilder();
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != ' ') {
+                sb.append(arr[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,5 +1,11 @@
 package Test;
-
+import Solution.Solution ;
 public class Test {
-
+public static void main(String[] args) {
+	
+	Solution s1 = new Solution();
+	int[] abc = new int[] {1, 16, 6, 15, 0, 10, 11, 3};
+	System.out.println(s1.solution("apporoograpemmemprs", abc));
+}
+	
 }
