705f1730142d4fb660f59b1d15f79ab62c672356
juseungBaek <qkrwn37@naver.com>
2024-03-16 19:09:57 +0900

    hi

---
+++ src/Test/Test2.java
@@ -0,0 +1,11 @@
+package Test;
+import Solution.Solution2 ;
+public class Test2 {
+public static void main(String[] args) {
+	
+	Solution2 s1 = new Solution2();
+	
+	System.out.println(s1.solution(10,3));
+}
+	
+}
