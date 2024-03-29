aeb25e8ba264f0b33ba7ecbd3b1a2e51e590052a
juseungBaek <151498425+juseungBaek@users.noreply.github.com>
2024-03-29 14:46:07 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my, String is) {
+        int answer = 0;
+        if(my.startsWith(is)){
+            answer=1;
+        }
+        return answer;
+    }
+}
